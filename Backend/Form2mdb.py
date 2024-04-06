9de76f41420be47e8d588ed18e3a81aaab8eae95
Weegeeday <simon.besner@me.com>
2024-04-06 08:40:18 -0400

    some fixes/more mangodb work

--- Backend/Form2mdb.py
+++ Backend/Form2mdb.py
@@ -3,9 +3,30 @@ import pymongo
 import json
 from pymongo import MongoClient
 #setup
-client = MongoClient('10.2.25.82', 27017)
+try:
+    c = open("./Backend/ip.txt","r")
+    ip = c.readline()
+    c.close()
+except FileNotFoundError:
+    c = open("./Backend/ip.txt","x")
+    l = input("whats the MangoDB ip? It will be saved in ip.txt")
+    c.write(l)
+    ip = l
+    c.close()
+
+client = MongoClient(ip, 27017)
 db = client['main']
 mentees = db['mentees']
 mentors = db['mentors']
+class Form2mdb:
+    
 
-data = FormsAPI.FormsResp.GetResp()
+    @staticmethod
+    def FormGetNparse(fid):
+        data = FormsAPI.FormsResp.GetResp(fid)
+        print(data)
+        #how can we get a python dict into MDB?
+        resp = data["responses"]
+        for x in resp:
+            if
+            mentors.insert_one(resp)
--- Backend/FormsAPITest.py
+++ Backend/FormsAPITest.py
@@ -1,4 +1,7 @@
 import FormsAPI
+import time
 print("id")
 d = input()
 print(FormsAPI.FormsResp.GetResp(d))
+time.sleep(2)
+print(FormsAPI.FormsResp.GetForm(d))
